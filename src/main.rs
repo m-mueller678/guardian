433e81567c8784af6a09bbfcd55dabe295b09b48
m-mueller678 <marcus.mueller.678@gmail.com>
2023-10-04 00:05:50 +0200

    server core logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use anyhow::{anyhow, Result};
-use fastwebsockets::upgrade;
+use anyhow::{anyhow, bail, ensure, Result};
 use fastwebsockets::OpCode;
+use fastwebsockets::{upgrade, WebSocketError};
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::Body;
@@ -10,26 +10,107 @@ use rustls::{Certificate, PrivateKey};
 use std::fs::File;
 use std::io::BufReader;
 use std::sync::Arc;
+use std::time::Duration;
+
 use tokio::net::TcpListener;
+use tokio::time::{sleep_until, Instant};
 use tokio_rustls::rustls;
 use tokio_rustls::TlsAcceptor;
 
 async fn handle_client(fut: upgrade::UpgradeFut) -> Result<()> {
+    const CONNECTION_TIMEOUT: u64 = 10;
+    const DEFUSE_TIMEOUT: u64 = 10;
+    const UPDATE_SIZE: usize = 17;
+
     let mut ws = fut.await?;
     ws.set_writev(false);
-    let mut ws = fastwebsockets::FragmentCollector::new(ws);
 
+    let mut initial_data = Vec::new();
     loop {
         let frame = ws.read_frame().await?;
         match frame.opcode {
-            OpCode::Close => break,
-            OpCode::Text | OpCode::Binary => {
-                ws.write_frame(frame).await?;
+            OpCode::Binary | OpCode::Continuation => {
+                initial_data.extend_from_slice(&frame.payload);
+                if frame.fin {
+                    break;
+                }
             }
-            _ => {}
+            _ => bail!("bad setup message {:?}", frame.opcode),
         }
     }
-
+    dbg!(initial_data.len());
+    let mut last_message = [u8::MAX; UPDATE_SIZE];
+    let mut defuse_timeout = None;
+    let mut connection_timeout = Instant::now() + Duration::from_secs(CONNECTION_TIMEOUT);
+    let alert = loop {
+        let timeout = std::iter::once(connection_timeout)
+            .chain(defuse_timeout.into_iter())
+            .min()
+            .unwrap();
+        let frame = tokio::select! {
+            frame = ws.read_frame() =>{
+                frame
+            }
+            _t = sleep_until(timeout) =>{
+                break true;
+            }
+        };
+        let frame = match frame {
+            Ok(frame) => frame,
+            Err(e @ (
+                WebSocketError::InvalidFragment |
+                WebSocketError::InvalidUTF8 |
+                WebSocketError::InvalidContinuationFrame |
+                WebSocketError::InvalidStatusCode |
+                WebSocketError::InvalidUpgradeHeader |
+                WebSocketError::InvalidConnectionHeader |
+                WebSocketError::InvalidCloseFrame |
+                WebSocketError::InvalidCloseCode |
+                WebSocketError::ReservedBitsNotZero |
+                WebSocketError::ControlFrameFragmented |
+                WebSocketError::PingFrameTooLarge |
+                WebSocketError::FrameTooLarge |
+                WebSocketError::InvalidSecWebsocketVersion |
+                WebSocketError::InvalidValue |
+                WebSocketError::MissingSecWebSocketKey |
+                WebSocketError::HTTPError(_) // should only occur during upgrade
+            )) => {
+                return Err(e.into());
+            }
+            Err(WebSocketError::ConnectionClosed | WebSocketError::UnexpectedEOF | WebSocketError::IoError(_)) => {
+                break true;
+            }
+        };
+        match frame.opcode {
+            OpCode::Close => {
+                break true;
+            }
+            OpCode::Binary => {
+                let payload = &*frame.payload;
+                ensure!(payload.len() == UPDATE_SIZE);
+                last_message.copy_from_slice(payload);
+                match payload[0] {
+                    0 => defuse_timeout = None,
+                    1 => {
+                        if defuse_timeout.is_none() {
+                            defuse_timeout =
+                                Some(Instant::now() + Duration::from_secs(DEFUSE_TIMEOUT));
+                        }
+                    }
+                    2 => {
+                        break true;
+                    }
+                    3 => {
+                        break false;
+                    }
+                    _ => bail!("unrecognized code"),
+                }
+                connection_timeout = Instant::now() + Duration::from_secs(CONNECTION_TIMEOUT);
+            }
+            _ => bail!("bad message {:?}", frame.opcode),
+        }
+    };
+    dbg!(alert, last_message);
     Ok(())
 }
 
